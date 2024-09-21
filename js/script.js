4379ebaf75661417e422dcb1dd0e957e7d703af4
Erika Burrola <erika.burrola@aideguru.com>
2024-09-21 15:27:34 -0700

    M2 Assigments -Eburrola

--- js/script.js
+++ js/script.js
@@ -1,3 +1,125 @@
-//STEP1
-//STEP2
-//STEP3
+
+//Exercise number 1
+let someMonth;
+function theMonth(){
+
+}
+let currentMonth;
+let summerMonth;
+let myLibraryfunction;
+// Exercise number 2
+let num=42;
+let greeting ="Hello";
+let isActive = true;
+let value = null;
+// Exercise number 3
+let a = 5;
+let b = 10;
+let resultOp = (a*2)+(b/2);
+
+let firstName = "Erika";
+let lastName = "Eburrola";
+let fullName = firstName +" " + lastName;
+
+// Exercise number 4 y 5
+let strFirstName;
+let strLastName;
+let strAddress;
+let strCity = "San Diego";
+let strState = "California";
+let strZipCode;
+let intYourAge;
+let strReferralSource;
+let boolMayWecontactyou = true;
+
+//Exercise number 6
+let numberClass = 4;
+let text = "Clases" ;
+let result = numberClass + text;
+
+console.log(result);
+
+//Exercise number 7
+let isTrue = true;
+let text2 = " is the answer";
+let firstResult = isTrue + text2;
+console.log(firstResult);
+
+let number = 10;
+let secondResult = number + isTrue;
+console.log(secondResult);
+
+//Exercise number 8
+let someString = "Who once said, only two things are infinite, the universe and human stupidity, and I'm not sure about the former."
+console.log(someString)
+
+//Exercise number 9
+let nullValue = null;
+console.log(nullValue);
+let undefinedValue;
+console.log(undefinedValue);
+//Exercise number 10
+let strExample = "Hello, world!"
+console.log(typeof strExample);
+let numExample = 42;
+console.log(typeof numExample);
+let boolExample = true;
+console.log(typeof boolExample);
+let objExample = {name: "Erika", age:42};
+console.log(typeof objExample)
+let underfinedExample;
+console.log(typeof underfinedExample);
+/* 
+//  Test those exercises running HTML5.html
+//Exercise number 11
+let myName ="Erika";
+let myLastName = "Burrola";
+alert("Hello "+myName + " "+ myLastName +", welcome to the JavaScript class!");
+//Exercise number 12
+let yourName ="Erika Burrola";
+alert("Hello "+ yourName +", welcome to the JavaScript class!");
+//Exercise number 13/
+let nameCom = "Erika Burrola";
+let course = "JavaScript";
+alert("Hello " + nameCom +", welcome to the "+ course + " class!");
+//Exercise number 14/
+alert("Hello " + nameCom +",\n Welcome to the "+ course + " class!");
+//Exercise number 15
+let nameprom = prompt("Please enter your name:");
+let courseProm = "JavaScript";
+alert("Hello " + nameprom +", welcome to the "+ courseProm + " class!");
+//Exercise number 16
+let nameprom2 = prompt("Please enter your name:");
+let courseProm2 = prompt("What class are you taking?");
+alert("Hello " + nameprom2 +", welcome to the "+ courseProm2 + " class!");
+*/
+//Exercise number 17
+let x=10;
+let y=20;
+let sum = x+y;
+console.log(sum);
+//Exercise number 18
+let x2= 20;
+x2+=20;
+console.log(x2);
+//Exercise number 19
+let x3=20;
+x3*=5;
+console.log(x3);
+//Exercise number 20/
+let x4=20/3;
+x4/=1;
+console.log(x4);
+//Exercise number 21
+let a2 = 10;
+let b2 = 20;
+let c2 = 15;
+let result5 = (a2 < b2) && (b2 > c2 ) && (c2===15);
+console.log(result5);
+//Exercise number 20
+let d = 30;
+let e = 25;
+let f = 30;
+let result6 = (d>=e)||(e<f)||(d!==30);
+console.log(result6);
+
